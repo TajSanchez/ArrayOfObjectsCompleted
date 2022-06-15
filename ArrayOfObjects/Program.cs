9e7147e0c93dc0c6f30eb3d5c5aef1dca7783ab0
Taj <tajmarie92@gmail.com>
2022-06-14 20:44:07 -0500

    Add project files.

---
+++ ArrayOfObjects/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ArrayOfObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car[] garage = new Car[4];
+
+            Car car1 = new Car("Ferarri");
+            Car car2 = new Car("BMW");
+            Car car3 = new Car("McClarren");
+            Car car4 = new Car("Aston Martin");
+
+            garage[0] = car1;
+            garage[1] = car2;
+            garage[2] = car3;
+            garage[3] = car4;
+
+            Console.WriteLine(garage[0].model);
+            
+
+        }
+
+        class Car
+        {
+            public String model;
+            
+            public Car(string model)
+            {
+                this.model = model;
+            }
+        }
+    }
+}
